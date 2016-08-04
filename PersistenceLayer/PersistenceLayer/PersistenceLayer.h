9664719889bc3bbb14fdcf2d890053ec63cc53a0
SoonSYJ <sunyujia1989@gmail.com>
2016-08-04 09:54:49 +0800

    add persistence layer and business logic layer

---
+++ PersistenceLayer/PersistenceLayer/PersistenceLayer.h
@@ -0,0 +1,19 @@
+//
+//  PersistenceLayer.h
+//  PersistenceLayer
+//
+//  Created by syj12 on 8/4/16.
+//  Copyright © 2016 syj12. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PersistenceLayer.
+FOUNDATION_EXPORT double PersistenceLayerVersionNumber;
+
+//! Project version string for PersistenceLayer.
+FOUNDATION_EXPORT const unsigned char PersistenceLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PersistenceLayer/PublicHeader.h>
+
+
